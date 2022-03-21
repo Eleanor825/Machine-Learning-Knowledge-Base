761d577cda9d6da59558f944e371a6a89e7f117d
Daniel Mao <danielmao2019@gmail.com>
2022-03-20 22:51:14 -0400

    code update for smooth L1 loss

--- code-base/detection/losses/smooth_l1_loss.py
+++ code-base/detection/losses/smooth_l1_loss.py
@@ -4,12 +4,24 @@ import tensorflow as tf
 class SmoothL1Loss(tf.losses.Loss):
 
     def __init__(self, delta):
+        """
+        Arguments:
+            delta (float): threshold for using absolute loss or squared loss.
+        """
         super(SmoothL1Loss, self).__init__(reduction="none")
-        self.delta = delta
+        self._delta = delta
 
     def call(self, y_true, y_pred):
+        """
+        Arguments:
+            y_true: 3-D tensor of shape (batch_size, , 4).
+            y_pred: 3-D tensor of shape (batch_size, , 4).
+        Returns:
+            reduced_loss: 1-D tensor of shape (batch_size,).
+        """
         diff = y_true - y_pred
         absolute = tf.abs(diff)
         squared = diff ** 2
-        loss = tf.where(tf.less(absolute, self.delta), 0.5 * squared, absolute - 0.5)
-        return tf.reduce_sum(loss, axis=-1)
+        loss = tf.where(tf.less(absolute, self._delta), 0.5 * squared, absolute - 0.5)
+        reduced_loss = tf.reduce_sum(loss, axis=-1)
+        return reduced_loss
