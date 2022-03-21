c7c5c4ce7d1dfeaeb05b9a2e77c7f5c456aa5c56
Daniel Mao <danielmao2019@gmail.com>
2022-03-20 21:11:52 -0400

    fixed mistake in computing smooth_l1_loss

--- code-base/detection/losses/smooth_l1_loss.py
+++ code-base/detection/losses/smooth_l1_loss.py
@@ -11,5 +11,5 @@ class SmoothL1Loss(tf.losses.Loss):
         diff = y_true - y_pred
         absolute = tf.abs(diff)
         squared = diff ** 2
-        loss = tf.where(tf.less(diff, self.delta), 0.5 * squared, absolute - 0.5)
+        loss = tf.where(tf.less(absolute, self.delta), 0.5 * squared, absolute - 0.5)
         return tf.reduce_sum(loss, axis=-1)
