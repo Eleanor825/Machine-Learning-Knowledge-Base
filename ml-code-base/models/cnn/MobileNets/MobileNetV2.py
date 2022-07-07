423f42e5720398b7eb2a2e253880fcdd0017cc20
Daniel Mao <danielmao2019@gmail.com>
2022-07-07 11:42:07 -0400

    Thu Jul 7 updates

--- ml-code-base/models/cnn/MobileNets/MobileNetV2.py
+++ ml-code-base/models/cnn/MobileNets/MobileNetV2.py
@@ -2,52 +2,58 @@ import tensorflow as tf
 
 
 class MobileNetV2(tf.keras.Model):
+    """
+    Reference: https://arxiv.org/abs/1801.04381.
+    """
 
     def __init__(self, output_dim, **kwargs):
         super(MobileNetV2, self).__init__(name="MobileNetV2", **kwargs)
         self._output_dim = output_dim
 
-    def _bottleneck_block(self, x, input_channels, output_channels,
-                          strides, expansion_factor):
+    def _bottleneck_block_v2(self, x, ich, och, strides, expansion_factor):
         """
         Arguments:
             x (tensor): input to the bottleneck block.
-            input_channels (int): number of input channels.
-            output_channels (int): number of output channels.
+            ich (int): number of input channels.
+            och (int): number of output channels.
             strides (int): strides for the depthwise convolutional layer.
             expansion_factor (float): expansion factor from num input channels to num intermediate channels.
         Returns:
             Processed input (x).
         """
         shortcut = x
-        intermediate_channels = input_channels * expansion_factor
-        relu6_conv = tf.keras.layers.Conv2D(
+        intermediate_channels = ich * expansion_factor
+        exp_conv = tf.keras.layers.Conv2D(
             filters=intermediate_channels, kernel_size=1, strides=1, padding="same",
         )
         depthwise_conv = tf.keras.layers.DepthwiseConv2D(
             kernel_size=3, strides=strides, padding="same",
         )
-        linear_conv = tf.keras.layers.Conv2D(
-            filters=output_channels, kernel_size=1, strides=1, padding="same",
+        out_conv = tf.keras.layers.Conv2D(
+            filters=och, kernel_size=1, strides=1, padding="same",
         )
         relu6 = tf.keras.layers.ReLU(
             max_value=6, negative_slope=0, threshold=0,
         )
-        x = relu6_conv(x)
+        x = exp_conv(x)
+        x = tf.keras.layers.BatchNormalization()(x)
         x = relu6(x)
         x = depthwise_conv(x)
+        x = tf.keras.layers.BatchNormalization()(x)
         x = relu6(x)
-        x = linear_conv(x)
-        if x.shape[-1] == shortcut.shape[-1]:
+        x = out_conv(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        # ??? one batch norm or 3?
+        if x.shape == shortcut.shape:
             x = tf.keras.layers.Add()([x, shortcut])
         return x
 
-    def _bottleneck_sequence(self, x, input_channels, output_channels, s, t, n):
+    def _bottleneck_sequence(self, x, ich, och, s, t, n):
         """
         Arguments:
             x (tensor): input to the bottleneck sequence.
-            input_channels (int): number of input channels.
-            output_channels (int): number of output channels.
+            ich (int): number of input channels.
+            och (int): number of output channels.
             s (int): strides for the first bottleneck block in the sequence.
             t (float): expansion factor for each bottleneck block in the sequence.
             n (int): number of bottleneck blocks in the sequence.
@@ -56,50 +62,33 @@ class MobileNetV2(tf.keras.Model):
         """
         for i in range(n):
             strides = s if i == 0 else 1
-            x = self._bottleneck_block(
-                x, input_channels=input_channels, output_channels=output_channels,
-                strides=strides, expansion_factor=t,
-            )
+            x = self._bottleneck_block_v2(x, ich=ich, och=och, strides=strides, expansion_factor=t)
         return x
 
     def call(self, x):
         x = tf.keras.layers.Conv2D(
             filters=32, kernel_size=3, strides=2, padding="same",
         )(x)
-        x = self._bottleneck_sequence(
-            x, input_channels=32, output_channels=16, s=1, t=1, n=1,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=16, output_channels=24, s=2, t=6, n=2,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=24, output_channels=32, s=2, t=6, n=3,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=32, output_channels=64, s=2, t=6, n=4,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=64, output_channels=96, s=1, t=6, n=3,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=96, output_channels=160, s=2, t=6, n=3,
-        )
-        x = self._bottleneck_sequence(
-            x, input_channels=160, output_channels=320, s=1, t=6, n=1,
-        )
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = self._bottleneck_sequence(x, ich=32, och=16, s=1, t=1, n=1)
+        x = self._bottleneck_sequence(x, ich=16, och=24, s=2, t=6, n=2)
+        x = self._bottleneck_sequence(x, ich=24, och=32, s=2, t=6, n=3)
+        x = self._bottleneck_sequence(x, ich=32, och=64, s=2, t=6, n=4)
+        x = self._bottleneck_sequence(x, ich=64, och=96, s=1, t=6, n=3)
+        x = self._bottleneck_sequence(x, ich=96, och=160, s=2, t=6, n=3)
+        x = self._bottleneck_sequence(x, ich=160, och=320, s=1, t=6, n=1)
         x = tf.keras.layers.Conv2D(
             filters=1280, kernel_size=1, strides=1, padding="same",
         )(x)
-        x = tf.keras.layers.AveragePooling2D(
-            pool_size=7, strides=1, padding="valid",
-        )(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.GlobalAveragePooling2D(keepdims=True)(x)
         x = tf.keras.layers.Conv2D(
             filters=self._output_dim, kernel_size=1, strides=1, padding="valid",
         )(x)
         x = tf.reshape(x, shape=(self._output_dim,))
         return x
 
-    def build(self, input_shape):
+    def build(self, input_shape=(224, 224, 3)):
         inputs = tf.keras.Input(shape=input_shape)
         outputs = self.call(inputs)
         return tf.keras.Model(inputs=inputs, outputs=outputs,
@@ -109,6 +98,6 @@ class MobileNetV2(tf.keras.Model):
 
 if __name__ == "__main__":
     model = MobileNetV2(output_dim=10)
-    model = model.build(input_shape=(224, 224, 3))
+    model = model.build()
     model.summary()
     # tf.keras.utils.plot_model(model, to_file="MobileNetV2.png", show_shapes=True)
