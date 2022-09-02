5e1373d02fea4cef47b381cf7f6f4f0ef47f9548
danielmao2019 <danielmao2019@gmail.com>
2022-09-01 22:41:01 -0400

    updates

--- code-cv/models/cnn/VGG.py
+++ code-cv/models/cnn/VGG.py
@@ -22,10 +22,6 @@ class VGGNet(tf.keras.Model):
             padding='same',
             activation='relu',
         )
-        self.max_pool_1 = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
-        )
         ##################################################
         self.conv_3 = tf.keras.layers.Conv2D(
             filters=128,
@@ -38,10 +34,6 @@ class VGGNet(tf.keras.Model):
             kernel_size=(3, 3),
             padding='same',
             activation='relu',
-        )
-        self.max_pool_2 = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
         )
         ##################################################
         self.conv_5 = tf.keras.layers.Conv2D(
@@ -62,10 +54,6 @@ class VGGNet(tf.keras.Model):
             padding='same',
             activation='relu',
         )
-        self.max_pool_3 = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
-        )
         ##################################################
         self.conv_8 = tf.keras.layers.Conv2D(
             filters=512,
@@ -84,10 +72,6 @@ class VGGNet(tf.keras.Model):
             kernel_size=(3, 3),
             padding='same',
             activation='relu',
-        )
-        self.max_pool_4 = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
         )
         ##################################################
         self.conv_11 = tf.keras.layers.Conv2D(
@@ -108,10 +92,6 @@ class VGGNet(tf.keras.Model):
             padding='same',
             activation='relu',
         )
-        self.max_pool_5 = tf.keras.layers.MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2),
-        )
         ##################################################
         self.flatten = tf.keras.layers.Flatten()
         self.dense_1 = tf.keras.layers.Dense(
@@ -128,43 +108,51 @@ class VGGNet(tf.keras.Model):
         )
         ##################################################
 
-    def call(self, intputs):
-        x = intputs
+    def call(self, x):
+        max_pool_layer = tf.keras.layers.MaxPool2D(
+            pool_size=(2, 2),
+            strides=(2, 2),
+        )
         ##################################################
         x = self.conv_1(x)
         x = self.conv_2(x)
-        x = self.max_pool_1(x)
+        x = max_pool_layer(x)
         ##################################################
         x = self.conv_3(x)
         x = self.conv_4(x)
-        x = self.max_pool_2(x)
+        x = max_pool_layer(x)
         ##################################################
         x = self.conv_5(x)
         x = self.conv_6(x)
         x = self.conv_7(x)
-        x = self.max_pool_3(x)
+        x = max_pool_layer(x)
         ##################################################
         x = self.conv_8(x)
         x = self.conv_9(x)
         x = self.conv_10(x)
-        x = self.max_pool_4(x)
+        x = max_pool_layer(x)
         ##################################################
         x = self.conv_11(x)
         x = self.conv_12(x)
         x = self.conv_13(x)
-        x = self.max_pool_5(x)
+        x = max_pool_layer(x)
         ##################################################
         x = self.flatten(x)
         x = self.dense_1(x)
         x = self.dense_2(x)
         x = self.dense_3(x)
         ##################################################
-        outputs = x
-        return outputs
+        return x
 
-    def model(self):
-        inputs = tf.keras.Input(
-            shape=(self.INPUT_HEIGHT, self.INPUT_WIDTH, self.INPUT_DEPTH))
+    def build(self, input_shape):
+        inputs = tf.keras.Input(shape=input_shape)
         outputs = self.call(inputs)
         return tf.keras.Model(inputs=inputs, outputs=outputs,
-                              name='VGGNet')
+                              name='VGGNet',
+                              )
+
+
+if __name__ == "__main__":
+    model = VGGNet()
+    model = model.build()
+    print(model.summary())
